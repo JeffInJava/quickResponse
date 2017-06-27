f12d52fe892286736d744681d2a6ea56a4fffecf
JeffInJava <java99@126.com>
2017-06-27 17:15:22 +0800

    version-4.1

--- src/controller/Qr.java
+++ src/controller/Qr.java
@@ -1,12 +1,14 @@
 package controller;
 
 public class Qr {
+	public static final String QR_STATUS="123";
 	public static void main(String[] args) {
 		String str[] = {"aa","bb","cc"};
 		for(String s:str){
 			System.out.println(s);
 			if("bb".equals(s)){
 				System.out.println("---------");
+				System.out.println(QR_STATUS);
 			}
 		}
 	}
